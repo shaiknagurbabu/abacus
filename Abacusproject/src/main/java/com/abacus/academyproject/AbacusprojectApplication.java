104b5c6e43b81f5820399870c3296888bedd610c
shaiknagurbabu <111447521+shaiknagurbabu@users.noreply.github.com>
2022-09-19 16:41:49 +0530

    abacus project

---
+++ Abacusproject/src/main/java/com/abacus/academyproject/AbacusprojectApplication.java
@@ -0,0 +1,13 @@
+package com.abacus.academyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbacusprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbacusprojectApplication.class, args);
+	}
+
+}
---
+++ Abacusproject/src/test/java/com/abacus/academyproject/AbacusprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abacus.academyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbacusprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

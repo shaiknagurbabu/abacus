33647a6121138e3de4596a420c6e07105b8a5295
shaiknagurbabu <111447521+shaiknagurbabu@users.noreply.github.com>
2022-09-29 15:53:41 +0530

    Delete AbacusPortel directory

--- AbacusPortel/src/main/java/com/abacus/portel/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.abacus.portel.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.abacus.portel.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
